e3f6ab719feaed17afff309e41ad0e07c4684ecb
ouali aghiles <ouali.aghiles@gmail.com>
2019-01-19 00:39:43 +0100

    end feature edit Todo

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -37,59 +37,85 @@ try {
 
 class ToDoList {
 
-    /**
-     * constructor - Description
-     *
-     * @param {HTMLElemnt} selector Description
-     *
-     * @returns {type} Description
-     */
-    constructor (selector) {
-      this.container = selector
-      this.input = this.container.querySelector('.js-input')
-      this.listToDos = this.container.querySelector('.js-todos')
+  /**
+   * constructor - Description
+   *
+   * @param {string} selector Description
+   *
+   * @returns {HTMLElement} Description
+   */
+  constructor (selector) {
+    this.container = selector
+    this.input = this.container.querySelector('.js-input')
+    this.listToDos = this.container.querySelector('.js-todos')
 
-      this.addTodo()
-    }
+    this.addTodo()
+    this.editToDo()
+  }
 
-    /**
-     * addTodo - Description
-     *
-     * @returns {type} Description
-     */
-    addTodo () {
-      let btnAction = this.container.querySelector('[data-action = "addToDo"]')
-      btnAction.addEventListener('click', () => {
-        let contentToDo = this.input.value
-         this.creatElementList(contentToDo)
-        console.log(newToDoElement);
+  /**
+   * addTodo - main function to add todo
+   *
+   * @returns {HTMLElement} The new todo
+   */
+  addTodo () {
+    let btnAdd = this.container.querySelector('[data-action = "addToDo"]')
+    btnAdd.addEventListener('click', () => {
+      let contentToDo = this.input.value
+      if (contentToDo !== "") {
+        if (!this.container.querySelector('.js-is-edited')) {
+          this.creatElementList(contentToDo)
+        } else {
+          let editedToDo = this.container.querySelector('.js-is-edited ')
+          let correcteToDo = editedToDo.querySelector('.js-txt-todo')
+          correcteToDo.innerText = contentToDo
+          editedToDo.classList.remove('js-is-edited')
+        }
         this.input.value = ''
+      } else {
+        return false
+      }
+    })
+  }
+  editToDo () {
+    let btnEdit = this.container.querySelectorAll('[data-action = "editToDo"]')
+    btnEdit.forEach(elem => {
+      elem.addEventListener('click', () => {
+        elem.parentNode.classList.add('js-is-edited')
+
+        let contentText = elem.parentNode.querySelector('.js-txt-todo')
+        this.input.value = contentText.innerHTML
+        console.log( contentText.innerHTML)
       })
-    }
+    })
 
+  }
 
-    /**
-     * creatElementList - Description
-     *
-     * @param {string} innerText Description
-     *
-     * @returns {HTMLElemnt} Description
-     */
-    creatElementList(innerText) {
-      let newToDo = document.createElement("a")
-      newToDo.classList.add('panel-block')
-      let newToDoIcon = document.createElement("span")
-      newToDoIcon.classList.add('panel-icon')
-      let newToDoIconFa = document.createElement("i")
-      newToDoIconFa.classList.add('fas')
-      newToDoIconFa.classList.add('fa-check')
-      let newText = document.createElement("span")
-      newText.innerHTML = innerText
-      newToDoIcon.appendChild(newToDoIconFa)
-      newToDo.appendChild(newToDoIcon)
-      newToDo.appendChild(newText)
-      this.listToDos.appendChild(newToDo)
-    }
+  /**
+   * creatElementList - create To-do container html
+   *
+   * @param {string} innerText Description
+   *
+   * @returns {HTMLElemnt} Description
+   */
+  creatElementList(innerText) {
+    let newToDo = document.createElement("a")
+    newToDo.classList.add('panel-block')
+
+    let newToDoIcon = document.createElement("span")
+    newToDoIcon.classList.add('panel-icon')
+
+    let newToDoIconFa = document.createElement("i")
+    newToDoIconFa.classList.add('fas')
+    newToDoIconFa.classList.add('fa-check')
+    let newText = document.createElement("span")
+    newText.classList.add('js-txt-todo')
+    newText.innerHTML = innerText
+    newToDoIcon.appendChild(newToDoIconFa)
+    newToDo.appendChild(newToDoIcon)
+    newToDo.appendChild(newText)
+    this.listToDos.prepend(newToDo)
+  }
 }
 
 new ToDoList(document.querySelector('.panel'))
